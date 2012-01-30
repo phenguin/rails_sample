0bfa404939e852065a69c37003203f93663e1528
Stephen Rigsby <rigsby@mit.edu>
2012-01-29 23:51:44 -0500

    blah blah

--- app/assets/javascripts/customglobal.js
+++ app/assets/javascripts/customglobal.js
@@ -1,1 +1,8 @@
-alert("This is gonna be real annoying")
+function showlayer(layer){
+    var myLayer = document.getElementById(layer);
+    if(myLayer.style.display=="none" || myLayer.style.display==""){
+        myLayer.style.display="block";
+    } else { 
+        myLayer.style.display="none";
+    }
+}
