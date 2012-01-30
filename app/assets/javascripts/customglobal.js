830a42eee86f28782c3f06159825c71c2e4b8f5a
Stephen Rigsby <rigsby@mit.edu>
2012-01-30 16:36:59 -0500

    tryin to get week shifter

--- app/assets/javascripts/customglobal.js
+++ app/assets/javascripts/customglobal.js
@@ -3,6 +3,7 @@
 
 
 
+/* this if for our nav bar */
 
 $(document).ready(function(){
         $('li.headlink').hover(
@@ -10,3 +11,50 @@ $(document).ready(function(){
             function() { $('ul', this).css('display', 'none'); });
 
         });
+
+/*
+ *http://tutorialzine.com/2009/09/simple-ajax-website-jquery/
+ * this will be for the little week transfer section */
+
+$(document).ready(function(){
+        checkURL();
+        $('ul li a').click(function (e){
+            checkURL(this.hash);
+      });
+
+var lasturl="";
+
+function checkURL(hash)
+{
+    if(!hash) hash=window.location.hash
+    if(hash != lasturl)
+    {
+        lasturl=hash;
+        loadPage(hash);
+    }
+}
+
+function loadPage(url)
+{
+    url=url.replace('#page','');
+
+    $('#loading').css('visibility', 'visible');
+
+    $.ajax({
+        type: "POST"
+        url: "load_page.php",
+        data: 'page='+url,
+        dataType: "html",   //expect html to be returned
+        success: function(msg){
+
+            if(parseInt(msg)!=0)    //if no errors
+        {
+        $('#pageContent').html(msg);    //load the returned html into pageContet
+        $('#loading').css('visibility','hidden');   //and hide the rotating gif
+        }
+        }
+
+    });
+
+}
+
